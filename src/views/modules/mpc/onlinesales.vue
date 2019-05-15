7710f2a9a029e8f5a4cba27939deca43649b7c0e
hanchen.wang <hanchen.wang@gmkholdings.com>
2019-05-15 11:36:38 +0800

    价格跟踪

---
+++ src/views/modules/mpc/onlinesales.vue
@@ -0,0 +1,352 @@
+<template>
+  <div class="mod-demo-echarts">
+    <el-form :inline="true" :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-date-picker v-model="dataForm.beginDate" type="date" placeholder="开始日期" clearable></el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-date-picker v-model="dataForm.endDate" type="date" placeholder="结束日期" clearable></el-date-picker>
+      </el-form-item>
+      <el-form-item prop="product">
+        <el-select v-model="dataForm.product" multiple placeholder="商品">
+          <el-option v-for="item in productList" :key="item.prodcode" :label="item.prodname" :value="item.prodcode"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="dataForm.merchant" multiple placeholder="商家">
+          <el-option v-for="item in merchantList" :key="item.merctcode" :label="item.merctname" :value="item.merctcode"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="dataForm.weight" multiple placeholder="规格">
+          <el-option v-for="item in specificationList" :key="item.speccode" :label="item.specname" :value="item.speccode"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="dataForm.platform" multiple placeholder="平台">
+          <el-option v-for="item in platformList" :key="item.platcode" :label="item.platname" :value="item.platcode"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-tabs>
+      <el-tab-pane label="列表">
+        <el-table
+          :data="tableDataList"
+          border
+          style="width: 100%;">
+          <el-table-column
+            prop="name"
+            header-align="center"
+            align="center"
+            label="名称">
+          </el-table-column>
+          <el-table-column
+            prop="price"
+            header-align="center"
+            align="center"
+            label="价格">
+          </el-table-column>
+          <el-table-column
+            prop="promotionPrice"
+            header-align="center"
+            align="center"
+            label="活动价格">
+          </el-table-column>
+          <el-table-column
+            prop="salesVolume"
+            header-align="center"
+            align="center"
+            label="销量">
+          </el-table-column>
+          <el-table-column
+            prop="salesDate"
+            header-align="center"
+            align="center"
+            label="销售日期">
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+
+      <el-tab-pane label="价格折线图">
+        <el-row :gutter="20">
+          <el-col :span="24">
+            <el-card>
+              <div id="J_chartLineBox" class="chart-box"></div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-tab-pane>
+
+      <el-tab-pane label="销量柱状图">
+        <el-row :gutter="20">
+          <el-col :span="24">
+            <el-card>
+              <div id="J_chartBarBox" class="chart-box"></div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
+
+
+  </div>
+</template>
+
+<script>
+  import Highcharts from 'highcharts'
+  let today = new Date()
+  let lastWeekTime = today.getTime() - 7 * 24 * 60 * 60 * 1000
+  let lastWeek = new Date(lastWeekTime)
+
+  export default {
+    data () {
+      return {
+        dataForm: {
+          beginDate: lastWeek.toLocaleDateString(),
+          endDate: today.toLocaleDateString(),
+          platform: '',
+          merchant: '',
+          product: '',
+          weight: ''
+        },
+        activeName : 'first',
+        tableDataList: [],
+        productList: [],
+        merchantList: [],
+        specificationList: [],
+        platformList: [],
+        chartLine: null,
+        dataRule: {
+          product : [{ required: true, message: '商品不能为空',trigger: 'blur' }]
+        }
+      }
+    },
+    created: function () {
+      this.getSelectOptions()
+    },
+
+    activated () {
+      // if (this.chartLine) {
+      //   this.chartLine.resize()
+      // }
+    },
+
+    methods: {
+      // 获取数据列表
+      getDataList () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl('/mpc/mpcOnlinesales/list2'),
+              method: 'get',
+              params: this.$http.adornParams({
+                'beginDate': this.dataForm.beginDate,
+                'endDate': this.dataForm.endDate,
+                'weight': this.dataForm.weight.toString(),
+                'platform': this.dataForm.platform.toString(),
+                'merchant': this.dataForm.merchant.toString(),
+                'product': this.dataForm.product.toString()
+              })
+            }).then(({data}) => {
+              if (data && data.code === 0) {
+                this.tableDataList = data.list
+                this.initChartLine(data)
+                this.initChartBar(data)
+              } else {
+                this.tableDataList = []
+                this.chartLine = Highcharts.chart(document.getElementById('J_chartLineBox'), {
+                  'title': {
+                    'text': '暂无数据'
+                  }
+                })
+                this.chartLine = Highcharts.chart(document.getElementById('J_chartBarBox'), {
+                  'title': {
+                    'text': '暂无数据'
+                  }
+                })
+              }
+            })
+          }
+        })
+      },
+      getSelectOptions () {
+        this.$http({
+          url: this.$http.adornUrl('/mpc/mpcOnlinesales/selectOptions'),
+          method: 'get'
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+            this.productList = data.productList
+            this.merchantList = data.merchantList
+            this.specificationList = data.specificationList
+            this.platformList = data.platformList
+          }
+        })
+      },
+      initChartLine (data) {
+        let seriesData = []
+        for (let i = 0; i < data.legendData.length; i++) {
+          let legend = data.legendData[i]
+          let lineList = data.reportList[legend]
+          let showData = []
+          for (let j = 0; j < lineList.length; j++) {
+            showData[j] = [this.stringToDate(lineList[j].salesDate), lineList[j].price]
+          }
+          seriesData[i] = {
+            'name': legend,
+            'data': showData
+          }
+        }
+        let option = {
+          chart: {
+            type: 'spline'
+          },
+          'title': {
+            'text': '价格跟踪折线图'
+          },
+          'legend': {
+            'data': data.legendData
+          },
+          tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
+            pointFormat: '{point.x:%Y-%m-%d}: ￥{point.y:.2f} '
+          },
+          'grid': {
+            'left': '3%',
+            'right': '4%',
+            'bottom': '3%',
+            'containLabel': true
+          },
+          'toolbox': {
+            'feature': {
+              'saveAsImage': { }
+            }
+          },
+          'xAxis': {
+            'type': 'datetime',
+            dateTimeLabelFormats: {
+              day: '%Y-%m-%d'
+            },
+            title: {
+              text: '日期'
+            },
+            softMin : this.stringToDate(this.dataForm.beginDate, '/'),
+            softMax : this.stringToDate(this.dataForm.endDate, '/')
+          },
+          'yAxis': {
+            title: {
+              text: '价格'
+            },
+            min: 0
+          },
+          'series': seriesData
+        }
+        this.chartLine = Highcharts.chart(document.getElementById('J_chartLineBox'), option)
+        // window.addEventListener('resize', () => {
+        //   this.chartLine.resize()
+        // })
+      },
+      initChartBar (data) {
+        let seriesData = []
+        for (let i = 0; i < data.legendData.length; i++) {
+          let legend = data.legendData[i]
+          let lineList = data.reportList[legend]
+          let showData = []
+          for (let j = 0; j < lineList.length; j++) {
+            showData[j] = [this.stringToDate(lineList[j].salesDate), lineList[j].salesVolume]
+          }
+          seriesData[i] = {
+            'name': legend,
+            'data': showData
+          }
+        }
+        let option = {
+          chart: {
+            type: 'column'
+          },
+          'title': {
+            'text': '价格跟踪折线图'
+          },
+          'legend': {
+            'data': data.legendData
+          },
+          tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
+            pointFormat: '{point.x:%Y-%m-%d} {point.y:.2f}'
+          },
+          'grid': {
+            'left': '3%',
+            'right': '4%',
+            'bottom': '3%',
+            'containLabel': true
+          },
+          'toolbox': {
+            'feature': {
+              'saveAsImage': { }
+            }
+          },
+          'xAxis': {
+            'type': 'datetime',
+            dateTimeLabelFormats: {
+              day: '%Y-%m-%d'
+            },
+            title: {
+              text: '日期'
+            },
+            softMin : this.stringToDate(this.dataForm.beginDate, '/'),
+            softMax : this.stringToDate(this.dataForm.endDate, '/')
+          },
+          'yAxis': {
+            title: {
+              text: '价格'
+            },
+            min: 0
+          },
+          'series': seriesData
+        }
+        this.chartLine = Highcharts.chart(document.getElementById('J_chartBarBox'), option)
+        // window.addEventListener('resize', () => {
+        //   this.chartLine.resize()
+        // })
+      },
+      stringToDate (dateStr, separator) {
+        if (!separator) {
+          separator = '-'
+        }
+        let dateArr = dateStr.split(separator)
+        let year = parseInt(dateArr[0])
+        let month
+        // 处理月份为04这样的情况
+        if (dateArr[1].indexOf('0') === 0) {
+          month = parseInt(dateArr[1].substring(1))
+        } else {
+          month = parseInt(dateArr[1])
+        }
+        let day = parseInt(dateArr[2])
+        let date = Date.UTC(year, month -1, day)
+        return date
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .mod-demo-echarts {
+    /*> .el-alert {*/
+      /*margin-bottom: 10px;*/
+    /*}*/
+    > .el-row {
+      margin-top: -10px;
+      margin-bottom: -10px;
+      .el-col {
+        padding-top: 10px;
+        padding-bottom: 10px;
+      }
+    }
+    .chart-box {
+      min-height: 400px;
+    }
+  }
+</style>
--- src/views/modules/mpc/onlinesalesprice.vue
+++ src/views/modules/mpc/onlinesalesprice.vue
@@ -9,7 +9,7 @@
       </el-form-item>
       <el-form-item>
         <el-select v-model="dataForm.product" multiple placeholder="商品">
-          <el-option v-for="item in productList" :key="item.value" :label="item.label" :value="item.value"></el-option>
+          <el-option v-for="item in productList" :key="item.prodcode" :label="item.prodname" :value="item.prodcode"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -34,12 +34,12 @@
     </el-form>
 
     <!--<el-alert-->
-      <!--title="提示："-->
-      <!--type="warning"-->
-      <!--:closable="false">-->
-      <!--<div slot-scope="description">-->
-        <!--<p class="el-alert__description">1. 此Demo只提供ECharts官方使用文档，入门部署和体验功能。具体使用请参考：http://echarts.baidu.com/index.html</p>-->
-      <!--</div>-->
+    <!--title="提示："-->
+    <!--type="warning"-->
+    <!--:closable="false">-->
+    <!--<div slot-scope="description">-->
+    <!--<p class="el-alert__description">1. 此Demo只提供ECharts官方使用文档，入门部署和体验功能。具体使用请参考：http://echarts.baidu.com/index.html</p>-->
+    <!--</div>-->
     <!--</el-alert>-->
 
     <el-row :gutter="20">
@@ -53,148 +53,149 @@
 </template>
 
 <script>
-import echarts from 'echarts'
-export default {
-  data () {
-    return {
-      dataForm: {
-        beginDate: '',
-        endDate: '',
-        platform: '',
-        merchant: '',
-        product: '',
-        weight: ''
-      },
-      dataList: [],
-      productList: [],
-      merchantList: [],
-      specificationList: [],
-      platformList: [],
-      chartLine: null
-    }
-  },
-  created: function () {
-    this.getSelectOptions()
-  },
+  import echarts from 'echarts'
+  let today = new Date()
+  let lastWeekTime = today.getTime() - 7 * 24 * 60 * 60 * 1000
+  let lastWeek = new Date(lastWeekTime)
 
-  activated () {
-    if (this.chartLine) {
-      this.chartLine.resize()
-    }
-  },
-
-  methods: {
-    // 获取数据列表
-    getDataList () {
-      debugger
-      this.$http({
-        url: this.$http.adornUrl('/mpc/mpcOnlinesales/list'),
-        method: 'get',
-        params: this.$http.adornParams({
-          'beginDate': this.dataForm.beginDate,
-          'endDate': this.dataForm.endDate,
-          'weight': JSON.stringify(this.dataForm.weight),
-          'platform': JSON.stringify(this.dataForm.platform),
-          'merchant': JSON.stringify(this.dataForm.merchant),
-          'product': JSON.stringify(this.dataForm.product)
-        })
-      }).then(({data}) => {
-        if (data && data.code === 0) {
-          this.dataList = data.list
-        } else {
-          this.dataList = []
-        }
-      })
-    },
-    getSelectOptions () {
-      this.$http({
-        url: this.$http.adornUrl('/mpc/mpcOnlinesales/selectOptions'),
-        method: 'get'
-      }).then(({data}) => {
-        if (data && data.code === 0) {
-          this.productList = data.productList
-          this.merchantList = data.merchantList
-          console.log(data.merchantList)
-          this.specificationList = data.specificationList
-          this.platformList = data.platformList
-        }
-        this.initChartLine()
-      })
-    },
-    initChartLine () {
-      var option = {
-        'title': {
-          'text': '价格跟踪折线图'
+  export default {
+    data () {
+      return {
+        dataForm: {
+          beginDate: lastWeek.toLocaleDateString(),
+          endDate: today.toLocaleDateString(),
+          platform: '',
+          merchant: '',
+          product: '',
+          weight: ''
         },
-        'tooltip': {
-          'trigger': 'axis'
-        },
-        'legend': {
-          'data': [ '邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎' ]
-        },
-        'grid': {
-          'left': '3%',
-          'right': '4%',
-          'bottom': '3%',
-          'containLabel': true
-        },
-        'toolbox': {
-          'feature': {
-            'saveAsImage': { }
-          }
-        },
-        'xAxis': {
-          'type': 'value'
-        },
-        'yAxis': {
-          'type': 'value'
-        },
-        'series': [
-          {
-            'name': '邮件营销',
-            'type': 'line',
-            'stack': '总量',
-            'data': [ 120, 132, 101, 134, 90, 230, 210 ]
-          },
-          {
-            'name': '联盟广告',
-            'type': 'line',
-            'stack': '总量',
-            'data': [ 220, 182, 191, 234, 290, 330, 310 ]
-          },
-          {
-            'name': '视频广告',
-            'type': 'line',
-            'stack': '总量',
-            'data': [ 150, 232, 201, 154, 190, 330, 410 ]
-          },
-          {
-            'name': '直接访问',
-            'type': 'line',
-            'stack': '总量',
-            'data': [ 320, 332, 301, 334, 390, 330, 320 ]
-          },
-          {
-            'name': '搜索引擎',
-            'type': 'line',
-            'stack': '总量',
-            'data': [ 820, 932, 901, 934, 1290, 1330, 1320 ]
-          }
-        ]
+        dataList: [],
+        productList: [],
+        merchantList: [],
+        specificationList: [],
+        platformList: [],
+        chartLine: null
       }
-      this.chartLine = echarts.init(document.getElementById('J_chartLineBox'))
-      this.chartLine.setOption(option)
-      window.addEventListener('resize', () => {
+    },
+    created: function () {
+      this.getSelectOptions()
+    },
+
+    activated () {
+      if (this.chartLine) {
         this.chartLine.resize()
-      })
+      }
+    },
+
+    methods: {
+      // 获取数据列表
+      getDataList () {
+        this.$http({
+          url: this.$http.adornUrl('/mpc/mpcOnlinesales/list'),
+          method: 'get',
+          params: this.$http.adornParams({
+            'beginDate': this.dataForm.beginDate,
+            'endDate': this.dataForm.endDate,
+            'weight': this.dataForm.weight.toString(),
+            'platform': this.dataForm.platform.toString(),
+            'merchant': this.dataForm.merchant.toString(),
+            'product': this.dataForm.product.toString()
+          })
+        }).then(({data}) => {
+          // debugger
+          console.log(data)
+          if (data && data.code === 0) {
+            // this.dataList = data.list
+            this.initChartLine(data)
+          } else {
+            this.dataList = []
+          }
+        })
+      },
+      getSelectOptions () {
+        this.$http({
+          url: this.$http.adornUrl('/mpc/mpcOnlinesales/selectOptions'),
+          method: 'get'
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+            this.productList = data.productList
+            this.merchantList = data.merchantList
+            this.specificationList = data.specificationList
+            this.platformList = data.platformList
+          }
+        })
+      },
+      initChartLine (data) {
+        let seriesData = []
+
+        for (let i = 0; i < data.legendData.length; i++) {
+          seriesData.push({
+            'type': 'line',
+            // 'seriesLayoutBy': 'column',
+            'connectNulls': true
+          })
+        }
+        let sourceData = []
+
+        for(let k =0; k < data.reportList.length; k ++){
+          // var sourceItem = '{'
+          // for (let j = 0; j < data.legendData.length; j++) {
+          //   debugger
+          //   sourceItem = sourceItem +  data.legendData[j] + ':' + data.reportList[k].map[data.legendData[j]] + ','
+          // }
+          // sourceItem =  sourceItem + '}
+          let sourceItem = JSON.stringify(data.reportList[k])
+          sourceData.push(sourceItem)
+        }
+        let dataset = {
+          dimensions: data.legendData,
+          source: sourceData
+        }
+        let option = {
+          'title': {
+            'text': '价格跟踪折线图'
+          },
+          'tooltip': {
+            // 'trigger': 'axis'
+          },
+          'legend': {
+            // 'data': data.legendData
+          },
+          'grid': {
+            'left': '3%',
+            'right': '4%',
+            'bottom': '3%',
+            'containLabel': true
+          },
+          'toolbox': {
+            'feature': {
+              'saveAsImage': { }
+            }
+          },
+          'xAxis': {
+            'type': 'category'
+          },
+          'yAxis': {
+
+          },
+          'dataset': dataset,
+          'series': seriesData
+        }
+        debugger
+        this.chartLine = echarts.init(document.getElementById('J_chartLineBox'))
+        console.log(option.toString())
+        this.chartLine.setOption(option)
+        window.addEventListener('resize', () => {
+          this.chartLine.resize()
+        })
+      }
     }
   }
-}
 </script>
 <style lang="scss">
   .mod-demo-echarts {
     /*> .el-alert {*/
-      /*margin-bottom: 10px;*/
+    /*margin-bottom: 10px;*/
     /*}*/
     > .el-row {
       margin-top: -10px;
